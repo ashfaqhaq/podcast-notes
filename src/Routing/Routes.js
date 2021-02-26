6fb5df1cb4ebd0dc10395cfa9d6de15f2d431977
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-02-27 05:17:43 +0530

    added homepage route

--- src/Routing/Routes.js
+++ src/Routing/Routes.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Dashboard from '../pages/Dashboard';
+import Dashboard from '../Pages/Dashboard';
+import Landing from '../Pages/Landing';
 function Routes() {
     return (
         <div>
             <BrowserRouter>
                   <Switch>
+                  <Route exact path="/" component={Landing} />
                   <Route  path="/dashboard" component={Dashboard} />
+                 
                     </Switch>
                     </BrowserRouter>
         </div>
