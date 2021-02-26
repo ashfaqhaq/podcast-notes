8a4076d0e1106de7547fa0ce3b5b7cbfa69ebf29
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-02-27 05:16:17 +0530

    space added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar() {
     return (
         <div>
-            <h3>This is a sidebar</h3>
+            <h3>This is a sidebar</h3> 
         </div>
     )
 }
