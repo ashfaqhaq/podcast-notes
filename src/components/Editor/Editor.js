ebc877c6ee3e52e93801dfd909f4e128f1974305
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-03-01 01:16:58 +0530

    reads spotify id from params

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -9,18 +9,22 @@ import { writeID, removeID } from '../../features/spotifyIdSlice';
 import _ from 'lodash';
 import { selectSpotifyID } from '../../features/spotifyIdSlice';
 import queryString from 'query-string'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
+import FlipMove from 'react-flip-move'
 function _Editor() {
     const location = useLocation()
-    const  episodeID  = queryString.parse(location.search)
-    console.log("episodeID",episodeID)
+    // const  episodeID  = queryString.parse(location.search)
+ 
 
     // const id = useSelector(selectSpotifyID)
-
+    
+    let { episodeID } = useParams()
+    console.log("episodeID",episodeID)
     const [content, setContent] = useState("");
     const [fileName, setFileName] = useState("");
     const [isLoaded, setIsLoaded] = useState(false);
-
+    const [h, setH] = useState(300)
+    const [w, setW] = useState(1000)
     const [unsaveChanges, setUnsaveChanges] = useState(false);
     const [noteID, setNoteID] = useState(null);
     const contentRef = useRef(null);
@@ -28,8 +32,8 @@ function _Editor() {
     const user = useSelector(selectUser)
 
     useEffect(() => {
-        if (user && user.uid) {
-            console.log(user.uid)
+        if (user && user.uid && episodeID) {
+            console.log("useEffect render",user.uid)
 
 
             // var uriInput = prompt('Please Enter your id')
@@ -38,17 +42,17 @@ function _Editor() {
             // };
             // console.log(spotify.uniqueURI.toString())
 
-            const noteDocRef = db.collection("users").doc(user.uid).collection("notes").doc(episodeID.id);
+            const noteDocRef = db.collection("users").doc(user.uid).collection("notes").doc(episodeID);
             //   console.log(await noteDocRef
             // const doc =  
             noteDocRef.get()
                 .then(doc => {
                     if (!doc.exists) {
                         noteDocRef.set({
-                            name: "SpotifyAudioTitle",
+                            name: "Placeholder text",
                             content: "",
                             createdAt: new Date(),
-                            id: episodeID.id
+                            id: episodeID
                         })
                         setIsLoaded(true)
                     } else {
@@ -60,7 +64,7 @@ function _Editor() {
                 });
             setNoteID(noteDocRef)
         }
-    }, [user,episodeID])
+    }, [user])
 
 
 
@@ -96,24 +100,28 @@ function _Editor() {
 
     return (
         <div> 
-                <iframe 
+            <div className="player">
+             <iframe 
             title="Spotify player"
-      src={`https://open.spotify.com/embed/episode/${episodeID.id}`}
-                 width="1000"
-                 height="300"
-             frameborder="0"
+            key="iframe"
+      src={`https://open.spotify.com/embed/episode/${episodeID}`}
+                 width={w}
+                 height={h}
+             frameborder="16"
                 allowtransparency="true"
                  allow="encrypted-media"
     />
+    </div>
            
           
                Notes Name <br/>
                 <input ref={fileNameRef} value={fileName} onChange={(e) => { setFileName(e.target.value) }} />
           <br/>
-           <button onClick={saveToDb} >
+           <button onClick={saveToDb} className="bg-green-500 text-white transition hover:scale-110 mx-3 p-3 rounded"  >
             Save
             </button>
-            
+            <input className="border bg-red" onChange={(e)=>setH(e.target.value)} />
+            <input  className="border bg-red" onChange={(e)=>setW(e.target.value)} />
             {
         isLoaded ?
                 <div className="m-10 px-10 py-5 border min-h-full">
@@ -145,4 +153,4 @@ function _Editor() {
     )
 }
 
-export default React.memo(_Editor)
+export default (_Editor)
