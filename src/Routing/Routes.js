a3ccdb99db417bbf3665ba449c0c9f39718ad4ad
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-02-28 06:04:55 +0530

    new routes

--- src/Routing/Routes.js
+++ src/Routing/Routes.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import { Switch } from 'react-router-dom';
+import { Switch,Redirect } from 'react-router-dom';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import GoogleSignIn from '../components/GoogleSignIn';
+import Signout from '../components/Signout';
 import Signup from '../components/Signup';
 import Dashboard from '../Pages/Dashboard';
 import Landing from '../Pages/Landing';
+import App from '../App.js'
 function Routes() {
     return (
         <div>
             <Router>
                   <Switch>
-                  <Route exact path="/" component={Landing} />
-                  <Route  exact path="/dashboard" component={Dashboard} />
+                  {/* <Route exact path="/" component={App}> */}
+                  <Route exact path="/" component={App} >
+                      
+                      
+                  <Route exact path="/dashboard" component={Dashboard} />
                   <Route exact path="/signup" component= {Signup}/>
+                  {/* <Route exact path="/signout" component= {Signout}/> */}
+                    
+                 
+                  </Route>
+                  {/* <Redirect path="/home" component={Landing}/> */}
             {/* <Route exact path="/Gsignin" component= {GoogleSignIn} /> */}
                   
                   </Switch>
