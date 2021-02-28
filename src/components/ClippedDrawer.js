de5fe378ca4bd468f63e2ddf71d78a27476f4880
iftekhar ul haq <ashfaq.28010@gmail.com>
2021-02-28 06:02:21 +0530

    sidebar

---
+++ src/components/ClippedDrawer.js
@@ -0,0 +1,110 @@
+import React,{useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import SideDrawer from './SideDrawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { Switch,Route, Link, BrowserRouter as Router } from 'react-router-dom'
+
+import Editor from '../components/Editor/Editor'
+import Notes from '../components/Notes'
+import Search from '../components/Search'
+// import InboxIcon from '@material-ui/icons/InboxIcon';
+import ExitToApp from '@material-ui/icons/ExitToApp';
+import MailIcon from '@material-ui/icons/Mail';
+import EpisodeList from './EpisodeList';
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function ClippedDrawer(props) {
+  const classes = useStyles();
+  console.log(props)
+   // eslint-disable-next-line no-unused-vars
+   
+
+ 
+    
+  return (
+    <div className={classes.root}>
+      <CssBaseline />  
+      
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Clipped drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <SideDrawer/>
+      <main className={classes.content}>
+        <Toolbar />
+        {/* <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+          donec massa sapien faucibus et molestie ac.
+        </Typography>
+        <Typography paragraph>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
+          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
+          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
+          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
+          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
+          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography> */}
+        <Switch>
+          <Route path="/notes">
+            <Notes />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/editor">
+            <Editor />
+          </Route>
+          <Route path="/episodes">
+            <EpisodeList />
+          </Route>
+        </Switch>
+      </main> 
+    </div>
+  );
+}
